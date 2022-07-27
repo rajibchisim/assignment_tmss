90f604692d19910a5cb9fe2e6575cd4300858495
rajibchisim <rajibjchisim@gmail.com>
2022-07-27 16:00:36 +0600

    Student Result sort test ..inprogress

--- tests/Feature/StudentResultSortTest.php
+++ tests/Feature/StudentResultSortTest.php
@@ -8,29 +8,84 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
+use AssertionError;
 
 class StudentResultSortTest extends TestCase
 {
     use RefreshDatabase;
 
     /** @test */
-    public function student_result()
+    public function student_sort_result()
     {
         $user = User::factory()->create();
         $this->actingAs($user);
         $student = Student::factory()->create();
 
-        $result = Result::factory()->create(['student_id' => $student->id, 'gpa' => 5, 'date' => '2022-07-15']);
-        Result::factory()->create(['student_id' => $student->id, 'gpa' => 4.5, 'date' => '2022-07-16']);
-        Result::factory()->create(['student_id' => $student->id, 'gpa' => 4, 'date' => '2022-07-17']);
-        Result::factory()->create(['student_id' => $student->id, 'gpa' => 3.5, 'date' => '2022-07-18']);
-        Result::factory()->create(['student_id' => $student->id, 'gpa' => 3, 'date' => '2022-07-19']);
-        Result::factory()->create(['student_id' => $student->id, 'gpa' => 2.5, 'date' => '2022-07-20']);
-        Result::factory()->create(['student_id' => $student->id, 'gpa' => 2, 'date' => '2022-07-21']);
+        $expected = [
+            [ 'student_id' => $student->id, 'date' => '2022-07-18', 'gpa' => 5 ],
+            [ 'student_id' => $student->id, 'date' => '2022-07-25', 'gpa' => 4.5 ],
+            [ 'student_id' => $student->id, 'date' => '2022-07-15', 'gpa' => 4 ],
+            [ 'student_id' => $student->id, 'date' => '2022-07-20', 'gpa' => 3.5 ],
+            [ 'student_id' => $student->id, 'date' => '2022-07-21', 'gpa' => 3 ],
+            [ 'student_id' => $student->id, 'date' => '2022-07-22', 'gpa' => 2.5 ],
+            [ 'student_id' => $student->id, 'date' => '2022-07-30', 'gpa' => 2 ],
+        ];
 
-        $response = $this->json('get', '/api/students/'.$student->id.'?results=true&&sort[gpa]=desc&&sort[date]=asc&&');
+        foreach ($expected as $result) {
+            Result::factory()->create($result);
+        }
 
-        $response->assertJsonFragment($result->toArray());
 
+        $response = $this->json('get', '/api/students/'.$student->id.'?results=true');
+
+        // $response->assertJsonFragment($studentsResultData);
+
+        $array = collect(
+            $response->json()['student']['results']['data'])
+            ->transform(function($item) {
+                return collect($item)->except(['id', 'created_at', 'updated_at'])->all();
+            })
+            ->all();
+
+            foreach ($expected as $item) {
+                $this->assertTrue(collect($array)->contains($item));
+            }
+
+            $response = $this->json('get', '/api/students/'.$student->id.'?results=true');
+            // &&sort[gpa]=desc&&sort[date]=asc&&
+            collect($expected)->sortBy('date', SORT_REGULAR, true)->sortBy('gpa')->dd();
+        // $this->assert
+
+    }
+
+
+    // works only for associative array
+    public function assertArraysAreEqualIgnoringKeyOrder(array $expected, array $array)
+    {
+        $this->assertEquals([], array_diff_key($expected, $array));
+        $this->assertEquals([], array_diff_key($array, $expected));
+
+        foreach ($expected as $key => $value) {
+            if (is_array($value)) {
+                $this->assertArraysAreEqualIgnoringKeyOrder($value, $array[$key]);
+            } else {
+                $this->assertEquals($value, $array[$key], "Array key '$key'");
+            }
+        }
+    }
+
+
+    public function assertArraysAreEqualIgnoringIndexOrder(array $expected, array $array)
+    {
+        $this->assertEquals([], array_diff_key($expected, $array));
+        $this->assertEquals([], array_diff_key($array, $expected));
+
+        foreach ($expected as $key => $value) {
+            if (is_array($value)) {
+                $this->assertArraysAreEqualIgnoringIndexOrder($value, $array[$key]);
+            } else {
+                $this->assertEquals($value, $array[$key], "Array key '$key'");
+            }
+        }
     }
 }
